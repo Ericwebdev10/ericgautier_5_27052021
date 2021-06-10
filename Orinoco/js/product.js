334f3062c5b175fb0343bb138c987914425e4294
ericwebdev <ericwebdev10@gmail.com>
2021-06-10 13:35:45 +0200

    Fix margin and NAV position

--- Orinoco/js/product.js
+++ Orinoco/js/product.js
@@ -7,11 +7,9 @@ function getProductDetails(){
     fetch(url + "/" + productId)
     .then( data => data.json())
     .then( article => {
-        console.log(article); // debug to delete
-        
+        console.log(article); // debug to delete       
         numberOfOption = article.varnish.length;   
-
-        document.querySelector(".row").innerHTML += `<div class="col-lg-6 col-md-6 mb-4 text-center">
+        document.querySelector(".row").innerHTML += `<div class="col-lg-6 col-md-6 mb-4 text-center mx-auto">
                                                         <div class="card h-100">
                                                             <a href="product.html?id=${article._id} "class=""><img class="card-img-top" src=${article.imageUrl} alt="image objet"/></a>
                                                             <div class="card-body">
@@ -32,15 +30,15 @@ function getProductDetails(){
                                                         </div>
                                                     </div>`;
 
-//loop to add options
-        i=0;
-        j=0; 
-        console.log(article.varnish.length); // debug to delete
-        console.log(article.varnish); // debug to delete
-        for (let i of article.varnish) {                                      
-            console.log("debug product id" + j + " " + i); // debug to delete
-            document.querySelector(".options").innerHTML += `<option value="${j}">${i}</option>`;
-            i++;
+        //loop to add options
+        ArticleOption = "";
+        j = 0; 
+//        console.log(article.varnish.length); // debug to delete
+//        console.log(article.varnish); // debug to delete
+        for (let ArticleOption of article.varnish) {                                      
+//            console.log("debug product id" + j + " " + ArticleOption); // debug to delete
+            document.querySelector(".options").innerHTML += `<option value="${j}">${ArticleOption}</option>`;
+            ArticleOption++;
             j++;
         };
     });
--- Orinoco/js/script.js
+++ Orinoco/js/script.js
@@ -2,7 +2,7 @@
 
 function displaySpecialOffer(visible){
     if (visible === true) {
-    document.querySelector(".row").innerHTML += `<div class="col-lg-12 text-center alert alert-info alert-dismissible fade show mt-3 shadow-lg" role="alert">
+    document.querySelector(".row").innerHTML += `<div class="col-lg-12 text-center alert alert-info alert-dismissible fade show mt-5 pt-5 shadow-lg" role="alert">
                                                     <h5 class="alert-heading">Offre spéciale : -20% avec le code Orinoco20</h5>
                                                     <p> + Livraison offerte !!! </p>
                                                     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
