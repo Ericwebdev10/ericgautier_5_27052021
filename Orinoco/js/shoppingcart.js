eea27b2122a0b964aac990dcc7ceb9348e7e42aa
ericwebdev <ericwebdev10@gmail.com>
2021-06-20 11:39:28 +0200

    SC  cards created

--- Orinoco/js/shoppingcart.js
+++ Orinoco/js/shoppingcart.js
@@ -1,5 +1,63 @@
 //----------------------------------------- Javascipt for shoppingcart.html page--------------------------------------------------
 
+//----------------------------------------- function to get localStorage content---------------------------------------------------------------
+function getItemsFromLocalStorage(response) {
+    arrayOfItemsInCart = JSON.parse(localStorage.getItem('itemsInCart')); //get back items array from localStorage
+    if (arrayOfItemsInCart === null) {                                    //case localStorage is blank
+        document.querySelector(".Items").innerHTML += `<h2>Le panier est vide</h2>`
+        response = "cart is empty";
+        console.log("debug id1 " + response);
+
+    } else{                                                               //case localStorage has 1 or more items
+        arrayOfItemsInCart.forEach(itemsInCart => {                       //loop to check if the same item with same varnish already exist in localStorage
+            createShoppingCartCards(itemsInCart)
+            //itemsInCart._id                
+            response = "cart is NOT empty";
+            console.log("debug id2 " + response);
+        });
+    };
+
+};
+
+//----------------------------------------- function to create card's content in the shopping cart section---------------------------------------------------------------
+function createShoppingCartCards(itemsInCart) {
+    document.querySelector(".Items").innerHTML += 
+        `<div class="product">
+            <div class="row border-bottom border-secondary">
+                <div class="col-md-3">
+                    <img class="img-fluid mx-auto d-block image" src="${itemsInCart.imageUrl}">
+                </div>
+                <div class="col-md-8">
+                    <div class="info">
+                        <div class="row">
+                            <div class="col-md-5 product-name">
+                                <div class="product-name">
+                                    <a href="#">${itemsInCart.name}</a>
+                                    <div class="product-info">
+                                        <div>Couleur: <span class="value">${itemsInCart.varnish}</span></div>
+                                        <div>Description: <span class="value">${itemsInCart.description}</span></div>
+                                        <div>ID: <span class="value">${itemsInCart._id}</span></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-3 price">
+                                <span>${itemsInCart.price} €</span>
+                            </div>
+                            <div class="col-md-4 quantity">
+                                <label for="quantity">Quantity:</label>
+                                <input id="quantity" type="number" value ="1" min="1" class="form-control quantity-input">
+                                <input type="button" class="btn btn-warning mt-5" value="Supprimer">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+            `
+};
+
+
+
 //----------------------------------------- Update element's values-----------------------------------------------------------------
 function updateElementText() {
 
@@ -34,3 +92,4 @@ function checkInputsValidity(response) {
 displayTotalQty();
 checkInputsValidity(false);
 updateElementText();
+getItemsFromLocalStorage();
